b90f0b1a687eda6553533b11edc128746db4ce4c
unknown <me@.(none)>
2011-06-04 15:41:38 -0400

    first commit

---
+++ src/com/Crash/Slots/SlotsPlugin.java
@@ -0,0 +1,24 @@
+package com.Crash.Slots;
+
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SlotsPlugin extends JavaPlugin {
+
+	@Override
+	public void onDisable() {
+		
+		
+		
+	}
+
+	@Override
+	public void onEnable() {
+		
+		PluginManager pm = getServer().getPluginManager();
+		
+		
+		
+	}
+
+}
